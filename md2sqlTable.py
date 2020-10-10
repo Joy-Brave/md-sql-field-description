3a7c13f78fe742306a080c22955189d468d3a9cc
Joy-Brave <yehgoter@yahoo.com.tw>
2020-10-10 16:52:00 +0800

    my code

--- md2sqlTable.py
+++ md2sqlTable.py
@@ -2,8 +2,9 @@ import config
 import myfunc
 #檔案存放路徑
 path="D:\\project\\md-sql-field-description\\result\\"
+#要Create 的表格 markdown檔清單
 filenamels=["SQL-Table-Employee_id.md"]
 sqlConn=myfunc.SqlConn(config.db)
 mdFieldDesc=myfunc.MdFieldDesc(path)
-#
+#DROP同名表格並重建
 sqlConn.createTable(mdFieldDesc.setMdTableDesc(filenamels).getMdTableDesc())
--- sqlTable2md.py
+++ sqlTable2md.py
@@ -6,7 +6,9 @@ path = "D:\\project\\md-sql-field-description\\result\\"
 mdFieldDesc=myfunc.MdFieldDesc(path)
 #資料庫相關class
 sqlConn=myfunc.SqlConn(config.db)
-#
+#將result folder中的現有Markdown檔案存取目前的欄位說明
 mdFieldDesc.setFilenameExtList('md').setMdTableDesc()
+#SELECT SQL 的欄位資訊
 sqlConn.selectTable(sqlConn.fieldDescQuery)
+#重新生成Markdown覆蓋舊檔，但同時保有舊檔案的欄位中文名稱以及詳細說明
 mdFieldDesc.createMdFromSqlTable(sqlConn.getSelectResult())
